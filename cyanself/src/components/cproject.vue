1297d3db81be4f5802772b9ea5f759f4ad64c066
thinkgamer <thinkgamer@163.com>
2018-10-23 00:31:13 +0800

    1231

---
+++ cyanself/src/components/cproject.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="procan">
+      <ul>
+        <li>项目一</li>
+        <li>项目一</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "project"
+    }
+</script>
+
+<style scoped>
+
+</style>
